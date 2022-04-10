890e7f4f1f03eefda8e1e316c4bbc7dd128e34c5
Farah Lubaba Rouf <farah.adrita@gmail.com>
2022-04-10 14:45:28 -0400

    smol

--- src/sample/DonutController.java
+++ src/sample/DonutController.java
@@ -9,19 +9,6 @@ import java.util.Arrays;
 
 public class DonutController {
 
-    @FXML
-    private ListView availableDonuts;
-
-    @FXML
-    private ListView selectedDonuts;
-
-    private static ArrayList<String> usualTypeYeast= new ArrayList<String>(
-            Arrays.asList("Glazed Chocolate", "Vanilla Creme", "Strawberry Frosted", "Jelly"));
-
-    private static ArrayList<String> usualTypeCake= new ArrayList<String>(
-            Arrays.asList("Chocolate", "Mango", "Sugar", "Jelly"));
-
-    private static ArrayList<String> usualTypeHole= new ArrayList<String>(
-            Arrays.asList("Glazed", "Vanilla", "Jelly", "Sugar"));
+    
 
 }
